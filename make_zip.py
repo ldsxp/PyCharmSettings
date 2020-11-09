1c616fefc91e603096c9ddfef6b08e7c506e762d
ldsxp <85176878@qq.com>
2020-11-09 15:36:31 +0800

    Update make_zip.py

--- make_zip.py
+++ make_zip.py
@@ -2,4 +2,4 @@ import shutil
 
 """生成导入使用的 settings.zip 文件"""
 
-ret = shutil.make_archive(base_name=r"settings", format='zip',root_dir=r"settings")
+ret = shutil.make_archive(base_name=r"build/settings", format='zip', root_dir=r"settings")
